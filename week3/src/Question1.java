570339dc1b07aa0222abb0c9683d9f55765955de
EnukaJ <enukaj8@gmail.com>
2022-10-12 14:39:41 +0530

    tutorial3

---
+++ week3/src/Question1.java
@@ -0,0 +1,7 @@
+public class Question1 {
+    public static void main(String[] args) {
+        String greeting = "Hello, World!";
+        int n = greeting.length();
+        String river = "Mississippi";
+    }
+}
