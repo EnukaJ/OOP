d90ae89fb77cb210af1bbd9d7b8d893cd2e75983
EnukaJ <enukaj8@gmail.com>
2022-10-19 13:16:07 +0530

    question 4

---
+++ week3/src/Question4.java
@@ -0,0 +1,18 @@
+public class Question4 {
+    public static void main(String[] args) {
+        public class VendingMachine{
+            int token;
+            int can;
+            public int tokens(){
+                int TokenNo =0;
+                return (TokenNo);
+            }
+            public int cans(){
+                int canNo =0;
+                return (canNo);
+            }
+
+        }
+
+    }
+}
