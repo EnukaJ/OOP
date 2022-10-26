7ee7bfcb32c57fccda5a3ea06c50750467e22db1
EnukaJ <enukaj8@gmail.com>
2022-10-26 08:31:59 +0530

    week 4

---
+++ week4/src/voltingMachine.java
@@ -0,0 +1,10 @@
+public class voltingMachine {
+    int labourVotes;
+    int conservativeVotes;
+    public void clearStatus(){
+        labourVotes = 0;
+        conservativeVotes = 0;
+    }
+
+
+}
